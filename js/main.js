17601871f876de2683d982a846b7f5ed1290e259
Ryohei Doi <ryo.do8675@gmail.com>
2017-01-14 15:11:59 +0900

    some fix

--- js/main.js
+++ js/main.js
@@ -1,36 +1,42 @@
-$(function({
+$(document).ready( function(){
   $.getJSON("shelves.json", function(json){
-    json.data.forEach(function(shelf){
+    var bs_count=0;
+    json[0].bookshelves.forEach(function(shelf){
+      var shelf_id = "shelf" + shelf.shelf_id;
       //bookshelfが複数の可能性があるためループ処理
-      $("#bookshelf").loadTemplate(
+      $("#bookshelves").loadTemplate(
         "bookshelves.template.html",{
           "shelf_name" : shelf.shelf_name,
           "shelf_owner" : shelf.shelf_owner,
-          "created_at" : shelf.created_at
-        }
+          "created_at" : shelf.created_at,
+          "bookshelf_id" : shelf_id
+        },
+        {append:true}
       );
 
+      console.log(shelf.books);
       //booksの表示のため別テンプレート呼び出し
-      json.data.books.forEach(function(book){
-        $("#books").loadTemplate(
+      shelf.books.forEach(function(book){
+        $("#bookshelves[shelf_id].books").loadTemplate(
             "books.template.html",{
               "isbn" : book.isbn,
               "name" : book.name,
               "file_name" : book.file_name,
               "comment" : book.comment
-            }
+            },
+            {append:true}
         );
-      });
+        //tagsの表示のため別テンプレート呼び出し
+        book.tags.forEach(function(tag){
+          $("#tags").loadTemplate(
+            "tags.template.html",{
+              "tag" : tag
+            },
+            {append:true}
+          );
+        }); // tags
+      });   // books
 
-      //tagsの表示のため別テンプレート呼び出し
-      json.data.tags.forEach(function(tag){
-        $("#tags").loadTemplate(
-          "tags.template.html",{
-            "tag" : tag
-          }
-        );
-      });
-
-    });
-  });
-}))
+    });     // bookshelves
+  });       // getJSON
+});
